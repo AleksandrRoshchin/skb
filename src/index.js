bc0e167df3ca2d923ac82f2ab9b96d8d865d098a
Aleksandr Roshchin <uitaleks@gmail.com>
2016-11-11 21:17:10 +0300

    add task 2B

--- src/index.js
+++ src/index.js
@@ -9,6 +9,31 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/task2A',(req, res) => {
+  const sum = (+req.query.a || 0) + (+req.query.b || 0);
+  res.send(sum.toString());
+});
+
+app.get('/task2B',(req, res) => {
+  const fullname = req.query.fullname;
+  const re = /^.*[\d_/-].*$/;
+  const arr = fullname.split(' ');
+  if (fullname === "" || fullname === " " || fullname.match(re) || arr.length > 3 || arr.length === 0){
+    res.send("Invalid fullname");
+  }
+  if (arr.length === 3) {
+    res.send(arr[2] +" " + arr[0].charAt(0) +". " + arr[1].charAt(0) + ".");
+  } else if (arr.length === 2) {
+    res.send(arr[1] +" " + arr[0].charAt(0) + ".");
+  } else if (arr.length === 1) {
+    res.send(arr[0]);
+  }
+
+
+
+
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
